e3ee04c4d2fe786813425068440e3b02ebf7bda7
PULZ3 <juan.lizarraga.05.2004@gmail.com>
2026-09-20 03:13:50 -0500

    Delete vehicles (1).js

--- vehicles (1).js
+++
@@ -1,9 +0,0 @@
-import { vehicles, getPublicVehicle } from "../_lib/vehicles.js";
-import { json } from "../_lib/http.js";
-
-export async function onRequestGet() {
-  return json({
-    ok: true,
-    vehicles: vehicles.map(getPublicVehicle)
-  });
-}
